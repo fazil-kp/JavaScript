065299ba22ced47aac259d8d72e846032c80aae5
Fazil <mfazilkp7@gmail.com>
2023-10-17 10:47:25 +0530

    if statement added

--- secondDay/array.js
+++ secondDay/array.js
@@ -26,3 +26,81 @@ var d = a.filter((data)=>{
     return data<10
 })
 console.log("filtered",d);
+
+
+// slice and splise 
+
+// slice will cut on the index value
+// splice will cut o the count
+
+
+const arr = [12,14,87,67,89,87,65,43]
+
+const sl=arr.slice(1,5) //print 89
+console.log('slice',sl);
+
+const sp=arr.slice(1,6) //print 6 counts in 1 st index
+console.log('splice',sp);
+
+
+
+// push (adding elemnt in last)
+// unshift (adding element in first)
+// pop (deleting last element)
+// shift (deleting first element)
+// some (in any condition is true that will true)
+// every (all condtn true) 
+
+const arry=[12,45,67,89,56,57,46,57]
+arry.push(99)
+console.log(arry);
+arry.unshift(87)
+console.log(arry);
+arry.pop()
+arry.shift()
+console.log(arry);
+
+const smev = arry.some((li)=>{
+    return li>12 // any cndtn is true then true
+})
+console.log(smev);
+
+const evsm = arry.some((li)=>{
+    return li>12 // all cndtn is true then true
+})
+console.log(evsm);
+
+// sort we can sort the array
+
+const srt = arry.sort((a,b)=>{
+    return a-b
+})
+console.log(srt);
+
+//includes the value is in the array or not
+
+const inc=[1,6,8,9,19,76]
+const inclu= inc.includes(8) // 8 in the array so true
+console.log(inclu);
+
+// join we can join the array in any format
+
+// const jo=[1,4,5,6,7,8]
+// const joi=jo.join(' + ')
+// console.log(joi);
+const jo=[1,4,5,6,7,8]
+const joi=jo.join(' + ')
+console.log(typeof(joi));
+
+
+// reverse we can write array in reverse format
+
+const re=[3,7,8,6,9,5,6,4]
+const rev = re.reverse()
+console.log(rev);
+
+// flat removing nested arrays in one array
+
+const f= [2,3,[3,4],5,[5,7,9],[9]]
+const fl=f.flat(3)
+console.log(fl);
---
+++ secondDay/statements.js
@@ -0,0 +1,13 @@
+// if
+
+var a = 500;
+var b= 800;
+var c = 300;
+
+if (a>b  && a>c){
+    console.log("a is big");
+}else if(b>a && b>c){
+    console.log("b is big");
+}else{
+    console.log('c is big');
+}
