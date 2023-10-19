a918e9c89f06c35d425077ffbb5f683b333fe9d0
Fazil <mfazilkp7@gmail.com>
2023-10-19 11:00:21 +0530

    4th Day updating

---
+++ fourthDay/accessingFiles/fileAccessing.js
@@ -0,0 +1,7 @@
+// var data = require('./method')
+// data.methods()
+// console.log(data);
+
+const {display1,display2} = require('./method')
+display1()
+display2()
---
+++ fourthDay/accessingFiles/method.js
@@ -0,0 +1,31 @@
+// const datas={
+//     name:'mfz',
+//     domain: 'Flutter',
+//     methods:function(){
+//         console.log('Hello Your Page is working');
+//     }
+// }
+
+// module.exports=datas
+
+
+
+
+// one function calling
+// function display(){
+//     console.log("Helloooooo");
+// }
+
+// module.exports=display
+
+// two function calling
+
+function display1(){
+    console.log("Helloooooo");
+}
+
+function display2(){
+    console.log("Hiiiiiiiiiii");
+}
+
+module.exports={display1,display2}
